32b9b3cfd7f421f5f877f59fcf2c4601a95ae4a1
sblack237 <sblack237@gmail.com>
2017-09-22 12:49:14 -0400

    Modifications to the seed data for generation of test data.

--- ProductCategory/Migrations/Configuration.cs
+++ ProductCategory/Migrations/Configuration.cs
@@ -24,6 +24,35 @@ namespace ProductCategory.Migrations
 
          });
 
+            context.Categories.AddOrUpdate(new Category[] {
+            new Category() { CategoryId = 1, CategoryName = "Science", CreatedOnUtc = DateTimeOffset.Parse("2017-10-22T04:50:12.0000000").UtcDateTime },
+            new Category() { CategoryId = 2, CategoryName = "Business", CreatedOnUtc = DateTimeOffset.Parse("2017-10-22T04:50:12.0000000").UtcDateTime },
+            
+           
+         });
+
+
+            var p1 = context.Products.Find(1);
+            var c1 = context.Categories.Find(1);
+
+            var p2 = context.Products.Find(2);
+            var c2 = context.Categories.Find(2);
+
+            context.ProductCategoryMappings.AddOrUpdate(new ProductCategoryMapping[] {
+            new ProductCategoryMapping() {
+                Id = 1,
+                Product = p1 ,
+                Category = c1 ,
+            },
+            new ProductCategoryMapping() {
+                Id = 2,
+                Product = p2 ,
+                Category = c2 ,
+            }
+
+         });
+
+
         }
     }
 }
